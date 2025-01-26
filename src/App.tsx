1766bad9376337c9c3af8d90fe51be970b6a9c17
Munir Shah <munirshahgillani1@gmail.com>
2025-01-26 15:35:06 +0500

    add app tsx file

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+export default function App() {
+  return (
+    <main className='bg-black'>
+      {/* <Navbar />
+      <Hero />
+      <Highlights /> */}
+    </main>
+  );
+}
